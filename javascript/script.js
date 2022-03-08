4f5655ec61516c7eb6bad02ca40a84bf0ec14723
Ciro Cusati <ciro.lavoro97@gmail.com>
2022-03-08 16:03:30 +0100

    reduced script

--- javascript/script.js
+++ javascript/script.js
@@ -3,27 +3,25 @@ const fizzBuzz = document.querySelector(".container");
 // loop from one to one hundred
 for (let i = 1; i <= 100; i++) {
     // if the number is a multiple of 3 and 5 enter this if
+    let print;
     if (i % 3 == 0 && i % 5 == 0) {
         console.log("fizzbuzz");
-        const print = `<div class="box fizz-buzz">fizzbuzz</div>`;
-        fizzBuzz.innerHTML += print;
+        print = `<div class="box fizz-buzz">fizzbuzz</div>`;
     }
     // if the number is a multiple of 3 enter this else if
     else if (i % 3 == 0) {
         console.log("fizz");
-        const print = `<div class="box fizz">fizz</div>`;
-        fizzBuzz.innerHTML += print;
+        print = `<div class="box fizz">fizz</div>`;
     }
     // if the number is a multiple of 5 enter this else if
     else if (i % 5 == 0) {
         console.log("buzz");
-        const print = `<div class="box buzz">buzz</div>`;
-        fizzBuzz.innerHTML += print;
+        print = `<div class="box buzz">buzz</div>`;
     }
     // if the number does not enter the withdrawals, it enters the else
     else {
         console.log(i);
-        const print = `<div class="box">${i}</div>`;
-        fizzBuzz.innerHTML += print;
+        print = `<div class="box">${i}</div>`;
     }
+    fizzBuzz.innerHTML += print;
 }
