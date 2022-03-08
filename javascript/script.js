683b7da8a5f812679f5ca7ac07fb4253d0a7c456
Ciro Cusati <ciro.lavoro97@gmail.com>
2022-03-08 14:13:14 +0100

    added layout + script

--- javascript/script.js
+++ javascript/script.js
@@ -1,2 +1,20 @@
-let prova = "Everything is Working";
-console.log(prova)
+const fizzBuzz = document.querySelector(".container");
+for (let i = 1; i <= 100; i++) {
+    if (i % 3 == 0 && i % 5 == 0) {
+        console.log("fizzbuzz");
+        const print = `<div class="box fizz-buzz">fizzbuzz</div>`;
+        fizzBuzz.innerHTML += print;
+    } else if (i % 3 == 0) {
+        console.log("fizz");
+        const print = `<div class="box fizz">fizz</div>`;
+        fizzBuzz.innerHTML += print;
+    } else if (i % 5 == 0) {
+        console.log("buzz");
+        const print = `<div class="box buzz">buzz</div>`;
+        fizzBuzz.innerHTML += print;
+    } else {
+        console.log(i);
+        const print = `<div class="box">${i}</div>`;
+        fizzBuzz.innerHTML += print;
+    }
+}
