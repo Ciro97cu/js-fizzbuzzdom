9090cbcadba740230a5d1811a137ced3c3d8038a
Ciro Cusati <ciro.lavoro97@gmail.com>
2022-03-08 14:58:38 +0100

    added comments

--- javascript/script.js
+++ javascript/script.js
@@ -1,18 +1,27 @@
+// variable that selects the class .container
 const fizzBuzz = document.querySelector(".container");
+// loop from one to one hundred
 for (let i = 1; i <= 100; i++) {
+    // if the number is a multiple of 3 and 5 enter this if
     if (i % 3 == 0 && i % 5 == 0) {
         console.log("fizzbuzz");
         const print = `<div class="box fizz-buzz">fizzbuzz</div>`;
         fizzBuzz.innerHTML += print;
-    } else if (i % 3 == 0) {
+    }
+    // if the number is a multiple of 3 enter this else if
+    else if (i % 3 == 0) {
         console.log("fizz");
         const print = `<div class="box fizz">fizz</div>`;
         fizzBuzz.innerHTML += print;
-    } else if (i % 5 == 0) {
+    }
+    // if the number is a multiple of 5 enter this else if
+    else if (i % 5 == 0) {
         console.log("buzz");
         const print = `<div class="box buzz">buzz</div>`;
         fizzBuzz.innerHTML += print;
-    } else {
+    }
+    // if the number does not enter the withdrawals, it enters the else
+    else {
         console.log(i);
         const print = `<div class="box">${i}</div>`;
         fizzBuzz.innerHTML += print;
